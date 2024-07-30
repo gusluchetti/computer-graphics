fbbbe88904498f4f389d6a99bd332e40cac1225e
Gustavo Luchetti <gustavo@luchetti.dev>
2024-07-30 17:52:12 -0300

    feat: init hello triangle!

--- vulkan/main.cpp
+++ vulkan/main.cpp
@@ -1,35 +1,35 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
-
+#include <cstdlib>
 #include <iostream>
 
-int main() {
-  glfwInit();
-
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow *window =
-      glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-  uint32_t extensionCount = 0;
-  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-  std::cout << extensionCount << " extensions supported\n";
-
-  glm::mat4 matrix;
-  glm::vec4 vec;
-  auto test = matrix * vec;
-
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
+class HelloTriangleApplication {
+public:
+  void run() {
+    initVulkan();
+    mainLoop();
+    cleanup();
   }
 
-  glfwDestroyWindow(window);
+private:
+  void initVulkan() {}
 
-  glfwTerminate();
+  void mainLoop() {}
 
-  return 0;
+  void cleanup() {}
+};
+
+int main() {
+  std::cout << "init main\n";
+  HelloTriangleApplication app;
+
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "quit main\n";
+  return EXIT_SUCCESS;
 }
