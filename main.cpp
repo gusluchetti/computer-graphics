d92050080670d77f2d655b118d168e391871f449
Gustavo Luchetti <gustavo@luchetti.dev>
2024-07-29 22:21:25 -0300

    chore: remove some abstractions

--- main.cpp
+++ main.cpp
@@ -13,52 +13,24 @@ SDL_GLContext glContext = nullptr;
 bool gQuit = false;
 
 const std::string gVertexShaderSource =
-    "#version 410 core"
-    "in vec4 position;"
-    "void main()"
-    "{"
-    "gl_Position = vec4(position.x, position.y, position.z, position.w);"
-    "}";
+    "#version 410 core\n"
+    "in vec4 position;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(position.x, position.y, position.z, position.w);\n"
+    "}\n";
 
 const std::string gFragmentShaderSource =
-    "#version 410 core"
-    "out vec4 color;"
-    "void main()"
-    "{"
-    "color = vec4(1.0f, 0.0f, 0.0f, 1.0f);"
-    "}";
+    "#version 410 core\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "color = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+    "}\n";
 
 GLuint gVertexArrayObject = 0;
 GLuint gVertexBufferObject = 0;
-
 GLuint gGraphicsPipelineShaderProgram = 0;
-
-void debugMessage(GLenum source, GLenum type, GLuint id, GLenum severity,
-                  GLsizei length, const GLchar *message,
-                  const void *userParam) {
-  // Print, log, whatever based on the enums and message
-  std::string message_str(message, length);
-  std::cout << message_str << '\n';
-}
-
-void VertexSpec() {
-  const std::vector<GLfloat> vertexPosition{-0.8f, -0.8f, 0.0f, 0.8f, -0.8f,
-                                            0.0f,  0.0f,  0.8f, 0.0f};
-
-  glGenVertexArrays(1, &gVertexArrayObject);
-  glBindVertexArray(gVertexArrayObject);
-
-  glGenBuffers(1, &gVertexBufferObject);
-  glBindBuffer(GL_ARRAY_BUFFER, gVertexBufferObject);
-  glBufferData(GL_ARRAY_BUFFER, vertexPosition.size() * sizeof(GLfloat),
-               vertexPosition.data(), GL_STATIC_DRAW);
-
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
-
-  // glBindVertexArray(0);
-  // glDisableVertexAttribArray(0);
-}
 
 GLuint CompileShader(GLuint type, const std::string &source) {
   std::string shaderSource;
@@ -71,19 +43,6 @@ GLuint CompileShader(GLuint type, const std::string &source) {
   return shaderObject;
 }
 
-void CreateGraphicsPipeline() {
-  GLuint programObject = glCreateProgram();
-  GLuint vertexShader = CompileShader(GL_VERTEX_SHADER, gVertexShaderSource);
-  GLuint fragmentShader =
-      CompileShader(GL_FRAGMENT_SHADER, gFragmentShaderSource);
-
-  glAttachShader(programObject, vertexShader);
-  glAttachShader(programObject, fragmentShader);
-  glLinkProgram(programObject);
-  glValidateProgram(programObject);
-  // glDetachShader, glDeleteShader;
-}
-
 void GetInfo() {
   std::cout << "Vendor: " << glGetString(GL_VENDOR) << "\n";
   std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
@@ -92,7 +51,44 @@ void GetInfo() {
             << "\n";
 }
 
-void InitProgram() {
+void MainLoop() {
+  while (!gQuit) {
+    SDL_Event e;
+
+    while (SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT) {
+        std::cout << "bye!\n";
+        gQuit = true;
+        break;
+      }
+    }
+
+    // pre draw
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_CULL_FACE);
+
+    glViewport(0, 0, gScreenWidth, gScreenHeight);
+    glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+    glUseProgram(gGraphicsPipelineShaderProgram);
+
+    // draw
+    glBindVertexArray(gVertexArrayObject);
+    glBindBuffer(GL_ARRAY_BUFFER, gVertexBufferObject);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    SDL_GL_SwapWindow(glWindow);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  std::cout << "at main\n";
+  std::cout << "init\n";
+
+  const std::vector<GLfloat> vertexPosition{-0.8f, -0.8f, 0.0f, 0.8f, -0.8f,
+                                            0.0f,  0.0f,  0.8f, 0.0f};
+
+  // init program
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     std::cout << "SDL2 could not initialize video subsys\n";
     exit(1);
@@ -124,68 +120,39 @@ void InitProgram() {
   }
 
   GetInfo();
-}
 
-void Input() {
-  SDL_Event e;
-
-  while (SDL_PollEvent(&e) != 0) {
-    if (e.type == SDL_QUIT) {
-      std::cout << "bye!\n";
-      gQuit = true;
-      break;
-    }
-  }
-}
-
-void PreDraw() {
-  glDisable(GL_DEPTH_TEST);
-  glDisable(GL_CULL_FACE);
-
-  glViewport(0, 0, gScreenWidth, gScreenHeight);
-  glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
-  glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-  glUseProgram(gGraphicsPipelineShaderProgram);
-}
-
-void Draw() {
+  // vertex spec
+  glGenVertexArrays(1, &gVertexArrayObject);
   glBindVertexArray(gVertexArrayObject);
+
+  glGenBuffers(1, &gVertexBufferObject);
   glBindBuffer(GL_ARRAY_BUFFER, gVertexBufferObject);
+  glBufferData(GL_ARRAY_BUFFER, vertexPosition.size() * sizeof(GLfloat),
+               vertexPosition.data(), GL_STATIC_DRAW);
 
-  glDrawArrays(GL_TRIANGLES, 0, 3);
-}
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
 
-void MainLoop() {
-  while (!gQuit) {
-    Input();
-    PreDraw();
-    Draw();
-    SDL_GL_SwapWindow(glWindow);
-  }
-}
+  glBindVertexArray(0);
+  glDisableVertexAttribArray(0);
 
-void Cleanup() {
+  GLuint programObject = glCreateProgram();
+  GLuint vertexShader = CompileShader(GL_VERTEX_SHADER, gVertexShaderSource);
+  GLuint fragmentShader =
+      CompileShader(GL_FRAGMENT_SHADER, gFragmentShaderSource);
+
+  glAttachShader(programObject, vertexShader);
+  glAttachShader(programObject, fragmentShader);
+  glLinkProgram(programObject);
+
+  glValidateProgram(programObject);
+
+  MainLoop();
+
+  // cleanup
   SDL_DestroyWindow(glWindow);
   SDL_Quit();
-}
 
-int main(int argc, char *argv[]) {
-  std::cout << "at main\n";
-  std::cout << "init\n";
-
-  InitProgram();
-
-  glEnable(GL_DEBUG_OUTPUT);
-  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-  glDebugMessageCallback(debugMessage, NULL);
-  glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL,
-                        GL_TRUE);
-
-  VertexSpec();
-  CreateGraphicsPipeline();
-  MainLoop();
-  Cleanup();
   std::cout << "quit\n";
-
   return 0;
 }
