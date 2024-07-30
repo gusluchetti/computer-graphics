a6bb9ffb5a41bbc30da678e16fb31d43aaf88f95
Gustavo Luchetti <gustavo@luchetti.dev>
2024-07-30 18:12:57 -0300

    feat: setup 01 base code

--- vulkan/main.cpp
+++ vulkan/main.cpp
@@ -1,22 +1,44 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <cstdlib>
 #include <iostream>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
   void run() {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
 private:
+  GLFWwindow *window;
+
+  void initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+
   void initVulkan() {}
 
-  void mainLoop() {}
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
 
-  void cleanup() {}
+  void cleanup() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
 };
 
 int main() {
